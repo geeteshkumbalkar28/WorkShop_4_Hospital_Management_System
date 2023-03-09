e21fb131f53e6efe604b2e0fb86dc4996de984a5
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-03-09 18:42:28 +0530

    Added : README file

--- ReadMe/md.java
+++ ReadMe/md.java
@@ -1,16 +1,23 @@
 Problem statement--
 
-        Program: Write a Java program to create a class called "Hospital" with instance variables "patients" (a list of Patient objects) and "numPatients" (the number of patients in the list). Include the following methods:
+        Program:Write a Java program to create a
+
+class called "Hospital"with instance variables"patients"(a list of Patient objects)and"numPatients"(the number of patients in the list).Include the following methods:
         A method to add a patient to the list
         A method to remove a patient from the list
         A method to search for a patient by their ID
-        A method to sort the patients in the list by their age (ascending or descending order)
+        A method to sort the patients in the list by their age(ascending or descending order)
         A method to calculate the average age of all patients
         A method to print the details of all patients in the hospital
 
-        Concepts used-
-        i have done it without using any inbuilt methods. Code is in logical concepts.i have implemented OOPS concepts here like
-        Inheritance is used.
-        Stram Api
-        Encapsulation is used.
-        Abstraction is used.
+        concept_used:: JAVA_BASICS,
+        OOPs,
+        STREAM_API,
+        JDBC,
+        MySQL,
+        DATA_STRUCTURE,
+        COLLECTOINS,
+        ANNOTATION,
+        DESIGN PATTERN,
+        REGEX,
+        EXCEPTION_HANDLING;
